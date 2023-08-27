6d313d5c075c3041e61a15882d854b536e7ce7ba
Egor Volkov <volkovqa96@gmail.com>
2023-08-27 13:54:43 +0300

    Описал формы

---
+++ mailer/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from mailer.models import MailingSettings, Client, MailingMessage
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'is_actual':
+                field.widget.attrs['class'] = 'form-control'
+
+
+class MailingSettingsForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = MailingSettings
+        fields = ('time', 'period', 'status', 'message')
+
+
+class ClientForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ('email', 'last_name', 'first_name', 'surname', 'comment')
+
+
+class MessageForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = MailingMessage
+        fields = ('subject', 'message')
